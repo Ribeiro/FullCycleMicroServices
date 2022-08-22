a178c662c9bf30826a1b0c5ea70f77ac31e81c11
Geovanny Ribeiro <geovanny.ribeiro@gmail.com>
2022-08-22 09:57:18 -0300

    Added tryc/catch block to disregard message not found when using in-memory repository

--- purchase/src/infra/consumer/TicketConsumer.ts
+++ purchase/src/infra/consumer/TicketConsumer.ts
@@ -5,7 +5,12 @@ export default class TicketConsumer {
 
     constructor(readonly queue: Queue, readonly confirmTicket: ConfirmTicket) {
         queue.consume("transactionApproved", async function (msg: any) {
-            await confirmTicket.execute(msg.externalCode);
+            //Added try/catch block in order to avoid flow break due to using in-memory repository
+            try {
+                await confirmTicket.execute(msg.externalCode);
+            } catch (error) {
+                console.log(error);
+            }
         });
     }
 
